c44dc3015a1f2a1b98adeeb3479054309201f4c6
ravinayak <ravinayak19@gmail.com>
2017-01-10 12:36:16 -0800

    Pushing array classes

---
+++ array_class/array_class.rb
@@ -0,0 +1,5 @@
+# Used for namespacing
+#
+module ArrayClass
+
+end
---
+++ array_class/initialization_methods.rb
@@ -0,0 +1,14 @@
+# Used for namespacing
+#
+module ArrayClass
+  # Includes initialization methods
+  #
+  module InitializationMethods
+    # Creates a new Array
+    #
+    def create_arr
+      arr = []
+      
+    end
+  end
+end
